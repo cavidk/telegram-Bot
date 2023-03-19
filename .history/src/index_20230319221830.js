0c0c6871b546f447169869ccd4560c555a9e4192
cavidk <cavid.kazimov.98@bk.ru>
2023-03-19 22:49:20 +0400

    [Main] change

---
+++ .history/src/index_20230319221830.js
@@ -0,0 +1,36 @@
+// const TelegramBot = require('node-telegram-bot-api');
+// require('dotenv').config();
+
+const TOKEN = process.env.TOKEN;
+
+
+
+//token registration
+
+const bot = new telegramBot(TOKEN, { polling: true});
+
+bot.on('message', (message)=>{
+
+   console.log(message);
+
+
+});
+
+//bot menu 
+
+bot.onText(/\/start/, (msg) => {
+   bot.sendMessage(msg.chat.id, "Welcome to my bot and Please choose an option from the menu:", {
+     reply_markup: {
+       keyboard: [
+         ['Option 1', 'Option 2'],
+         ['Option 3', 'Option 4'],
+         ['Option 5']
+       ]
+     }
+   });
+ });
+
+
+//  bot.onText(/Option 1/, (msg) => {
+//    bot.sendMessage(msg.chat.id, "You chose Option 1");
+//  });
---
+++ .history/src/index_20230319222214.js
@@ -0,0 +1,38 @@
+const TelegramBot = require('node-telegram-bot-api');
+//require('dotenv').config();
+
+const TOKEN = process.env.TOKEN;
+
+
+
+//token registration
+
+
+TOKEN = '5971983153:AAFLCNSYZWZoi_VI1zgs2VjDpiZMzesyDsI'
+const bot = new telegramBot(TOKEN, { polling: true});
+
+bot.on('message', (message)=>{
+
+   console.log(message);
+
+
+});
+
+//bot menu 
+
+bot.onText(/\/start/, (msg) => {
+   bot.sendMessage(msg.chat.id, "Welcome to my bot and Please choose an option from the menu:", {
+     reply_markup: {
+       keyboard: [
+         ['Option 1', 'Option 2'],
+         ['Option 3', 'Option 4'],
+         ['Option 5']
+       ]
+     }
+   });
+ });
+
+
+//  bot.onText(/Option 1/, (msg) => {
+//    bot.sendMessage(msg.chat.id, "You chose Option 1");
+//  });
